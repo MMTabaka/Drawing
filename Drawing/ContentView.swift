159667c05d0d4f31e2df2b061c3566c20b37b29e
MMTabaka <miloszmtabaka@gmail.com>
2022-04-18 22:54:03 +0100

    Initial Commit

---
+++ Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Milosz Tabaka on 18/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
